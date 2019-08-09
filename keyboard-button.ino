3a6633940a4a030851014c7daff17d0aa916d6f2
Grégory Paul <paulgreg@gmail.com>
2019-08-09 20:11:50 +0200

    Small updates about spaces and comments

--- keyboard-button.ino
+++ keyboard-button.ino
@@ -4,8 +4,6 @@
 #define MOD_CMD_LEFT 0x00000008
 #define DELAY 500
 
-// https://digistump.com/board/index.php?topic=1145.0
-
 void setup() {
   pinMode(1, OUTPUT); // LED
   pinMode(2, INPUT); // Button
@@ -24,10 +22,10 @@ void loop() {
   DigiKeyboard.update();
 }
 
-void blink() { 
+void blink() {
   digitalWrite(1, HIGH);
   DigiKeyboard.delay(CHAR_DELAY);
-  digitalWrite(1, LOW); 
+  digitalWrite(1, LOW);
 }
 
 void openPasswordManager() {
@@ -41,9 +39,9 @@ void openPasswordManager() {
 
 void computeAndPaste() {
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  
+
   DigiKeyboard.delay(DELAY);
-  
+
   DigiKeyboard.sendKeyStroke(KEY_V, MOD_CONTROL_LEFT);
   DigiKeyboard.delay(CHAR_DELAY);
   DigiKeyboard.sendKeyStroke(KEY_V, MOD_CMD_LEFT); // For Mac OS
